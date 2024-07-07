254b07f1c429629cc1c20e2a1dbfcb34505ccf83
Jozef_Zitt <dodo.zitt@gmail.com>
2024-07-07 13:58:26 +0200

    added parties

--- invoicepdf/src/components/PdfComponent.tsx
+++ invoicepdf/src/components/PdfComponent.tsx
@@ -1,14 +1,45 @@
+/* eslint-disable no-octal */
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
-// import 'pdfmake/build/vfs_fonts';
-
 
 export default function PdfComponent() {
-const userData = {
-    invoiceNumber: 2024001,
+
+interface Customer {
+    name: string,
+    street: string,
+    city: string,
+    ZIP: number,
+    country: string,
+    ICO: number,
+    DIC: number
+}
+interface Provider extends Customer {
+    invoiceNumber: number,
+    registered: string
 }
 
+const provider: Provider = {
+    invoiceNumber: 2024001,
+    name: 'Mgr. Jozef Žitt',
+    street: 'Jána Hollého 1056/5',
+    city: 'Michalovce',
+    ZIP: 71001,
+    country: 'Slovensko',
+    ICO: 55902227,
+    DIC: 1120397267,
+    registered: 'Okresný úrad Michalovce, Číslo živnostenského registra: 840-31636'
+}
+
+const customer: Customer = {
+    name: 'Company, s. r. o.',
+    street: 'Jarná 48',
+    city: 'Bratislava',
+    ZIP: 81109,
+    country: 'Slovensko',
+    ICO: 49081240,
+    DIC: 4564367832
+}
 
 
 
@@ -16,19 +47,67 @@ const userData = {
         const docDefinition = {
             content: [
                 {
-                    text: `Faktúra číslo: ${userData.invoiceNumber}`,
-                    style: 'header'
+                    text: `Faktúra číslo: ${provider.invoiceNumber}`,
+                    style: 'h1'
                 },
-                'Example'
+                {
+                    alignment: 'justify',
+                    columns: [
+                        {
+                            text: 'DODÁVATEĽ:',
+                            style: 'h2'
+                        },
+                        {
+                            text: 'ODBERATEĽ:',
+                            style: 'h2'
+                        },
+                    ],
+                },
+                {
+                    columns: [
+                        {
+                            text: `${provider.name}
+                            ${provider.street}
+                            ${provider.ZIP} ${provider.city}
+                            ${provider.country}
+                            
+                            IČO: ${provider.ICO}
+                            DIČ: ${provider.DIC}
+                            ${provider.registered}`,
+                        },
+                        {
+                            text: `${customer.name}
+                            ${customer.street}
+                            ${customer.ZIP} ${customer.city}
+                            ${customer.country}
+
+                            IČO: ${customer.ICO}
+                            DIČ: ${customer.DIC}`
+                        }, 
+                    ]
+                }
+           
+
             ],
             styles: {
-                header: {
-                    fontsize: 18,
+                h1: {
+                    alignment: 'center',
+                    bold: true,
+                    margin: [0, 0, 0, 20],
+                    fontSize: 15,
+                },
+                h2: {
                     bold: true,
                     margin: [0, 0, 0, 10]
                 }
             }
         }
+
+
+
+
+
+
         // pdfMake.createPdf(docDefinition).download('Invoice number: 2024001'); 
         // pdfMake.createPdf(docDefinition).print();
         pdfMake.createPdf(docDefinition).open();
