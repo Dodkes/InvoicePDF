88270600bd7a917378704616a578a7ab4a246efd
Jozef_Zitt <dodo.zitt@gmail.com>
2024-07-07 22:31:02 +0200

    formik initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import PdfComponent from './components/PdfComponent';
+import InvoiceForm from './components/InvoiceForm';
 
 function App() {
   return (
     <div className="App">
+      <InvoiceForm />
       <PdfComponent />
     </div>
   )
---
+++ src/components/InvoiceForm.tsx
@@ -0,0 +1,52 @@
+import { Formik, Field, ErrorMessage, Form } from 'formik'
+import * as Yup from 'yup'
+
+interface FormValues {
+  email: string
+  password: string
+}
+
+export default function InvoiceForm() {
+  const initialValues: FormValues = { email: '', password: '' }
+
+  return (
+    <div>
+        <h1>Fakturačné údaje</h1>
+        <Formik
+        initialValues={initialValues}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email('Neplatná emailová adresa')
+            .required('Povinné'),
+          password: Yup.string()
+            .min(8, 'Heslo musí mať aspoň 8 znakov')
+            .required('Povinné')
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+                alert(JSON.stringify(values, null, 2))
+                setSubmitting(false)
+            }, 400)
+        }}
+        >
+            {({ isSubmitting }) => (
+         <Form>
+           <div>
+             <label htmlFor="email">Email</label>
+             <Field type="email" name="email" placeholder="Váš email" />
+             <ErrorMessage name="email" component="div" />
+           </div>
+           <div>
+             <label htmlFor="password">Heslo</label>
+             <Field type="password" name="password" placeholder="Vaše heslo" />
+             <ErrorMessage name="password" component="div" />
+           </div>
+           <button type="submit" disabled={isSubmitting}>
+             Odoslať
+           </button>
+         </Form>
+       )}
+        </Formik>
+    </div>
+  )
+}
