ef50abb7b574c829b00af57da0d0653218b8967e
Jozef_Zitt <dodo.zitt@gmail.com>
2024-08-18 15:21:38 +0200

    db.json data dependency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,55 +1,68 @@
-import './App.css';
-import LoginForm from './components/LoginForm';
-import AccountDashboard from './components/AccountDashboard';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import LoginForm from "./components/LoginForm";
+import AccountDashboard from "./components/AccountDashboard";
+import { useState, useEffect } from "react";
 
 export interface FormValues {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
 
 export interface BackendData extends FormValues {
-  name: string
+  name: string;
 }
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [authenticationFailed, setAutenthicationFailed] = useState<boolean>(false)
-  const [backendData, setBackendData] = useState<BackendData[]>([])
-  const [activeUser, setActiveUser] = useState<object>({})
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [authenticationFailed, setAutenthicationFailed] =
+    useState<boolean>(false);
+  const [backendData, setBackendData] = useState<BackendData[]>([]);
+  const [activeUser, setActiveUser] = useState<object>({});
 
-async function getAPI() {
-    const response = await fetch('/api')
-    const jsonData = await response.json()
+  async function getAPI() {
+    const response = await fetch("/api");
+    const jsonData = await response.json();
 
-    setBackendData(jsonData)
-}
-
-useEffect(() => {
-  getAPI()
-}, [])
-
-function login (values: FormValues) {
-  const findInDB = backendData.filter((user: FormValues) => user.email === values.email && user.password === values.password)
-  if (findInDB.length !== 0) {
-    setIsLoggedIn(true)
-    setActiveUser(findInDB[0])
-    setAutenthicationFailed(false)
-  } else {
-    setAutenthicationFailed(true)
+    setBackendData(JSON.parse(jsonData));
+    console.log(JSON.parse(jsonData));
   }
-}
+
+  useEffect(() => {
+    getAPI();
+  }, []);
+
+  function login(values: FormValues) {
+    const findInDB = backendData.filter(
+      (user: FormValues) =>
+        user.email === values.email && user.password === values.password
+    );
+    console.log(findInDB);
+    if (findInDB.length !== 0) {
+      setIsLoggedIn(true);
+      setActiveUser(findInDB[0]);
+      setAutenthicationFailed(false);
+    } else {
+      setAutenthicationFailed(true);
+    }
+  }
 
   return (
     <div className="App">
-      {!isLoggedIn 
-      ? 
-        <LoginForm login={login} setIsLoggedIn={setIsLoggedIn} backendData={backendData} /> 
-      : 
-        <AccountDashboard setIsLoggedIn={setIsLoggedIn} signedUser={activeUser} />}
+      {!isLoggedIn ? (
+        <LoginForm
+          login={login}
+          setIsLoggedIn={setIsLoggedIn}
+          backendData={backendData}
+        />
+      ) : (
+        <AccountDashboard
+          setIsLoggedIn={setIsLoggedIn}
+          signedUser={activeUser}
+        />
+      )}
       {authenticationFailed ? <p>Authentication failed !</p> : null}
     </div>
-  )
+  );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -10,39 +10,11 @@ const app = express()
 const port = 8080
 app.use(express.json())
 
-const users = [
-  {
-    email: 'roth.malder@email.com',
-    password: 'roth.malder@email.comroth.malder@email.com',
-    organisation: 'Company Ltd.',
-    name: 'Roth Malder',
-    street: 'Jána Hollého 1056/5',
-    city: 'Michalovce',
-    ZIP: 71001,
-    country: 'Slovensko',
-    ICO: 55902227,
-    DIC: 1120397267,
-    registered: 'Okresný úrad Michalovce, Číslo živnostenského registra: 840-31636',
-    IBAN: 'SK72 2222 0000 0029 6259 7873',
-  },
-  {
-    email: "john.doe@email.sk",
-    password: "john.doe@email.skjohn.doe@email.sk",
-    name: "John Doe"
-  },
-  {
-    email: "jane.doe@email.eu",
-    password: "jane.doe@email.eujane.doe@email.eu",
-    name: "Jane Doe"
-  }
-]
-
 app.get('/api', (req, res) => {
-  res.json(users)
-
-  // fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
-  //   res.json(data)
-  // })
+  fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+    res.json(data)
+    console.log(data)
+  })
 })
 
 app.post('/api', (req, res) => {
